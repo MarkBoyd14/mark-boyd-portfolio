557fb90f94c11e63655a463a0090173fb3f09265
Mark Boyd <mark.boyd1414@gmail.com>
2021-03-06 14:07:38 -0500

    added routes to server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
-const path = require('path');
 const express = require('express');
 const app = express();
+const path = require('path');
 const publicPath = path.join(__dirname, '../build');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.get('/resume', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.get('/projects', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.get('/contact', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
