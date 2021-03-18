be045499e0f0228733a023fcb93f08f85839561d
Mark Boyd <mark.boyd1414@gmail.com>
2021-03-18 19:40:26 -0400

    improved footer layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,53 +4,55 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
   return (
     <footer id="footer">
-      <nav id="footer-nav">
-        <ul>
-          <li>
-            <Link to="/">About</Link>
-          </li>
-          <li>
-            <Link to="/resume">Resume</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <div id="footer-contact">
-        <ul>
-          <li>
-            <a
-              href="mailto: mark.boyd1414@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-envelope footer-contact-icon"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/mark-t-boyd/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-linkedin footer-contact-icon"></i>
-            </a>
-          </li>
+      <div id="footer-wrapper">
+        <nav id="footer-nav">
+          <ul>
+            <li>
+              <Link to="/">About</Link>
+            </li>
+            <li>
+              <Link to="/resume">Resume</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <div id="footer-contact">
+          <ul>
+            <li>
+              <a
+                href="mailto: mark.boyd1414@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-envelope footer-contact-icon"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/mark-t-boyd/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-linkedin footer-contact-icon"></i>
+              </a>
+            </li>
 
-          <li>
-            <a
-              href="https://github.com/MarkBoyd14?tab=repositories"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-github footer-contact-icon"></i>
-            </a>
-          </li>
-        </ul>
+            <li>
+              <a
+                href="https://github.com/MarkBoyd14?tab=repositories"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-github footer-contact-icon"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
